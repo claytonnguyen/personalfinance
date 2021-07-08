1b47624e7f42b1c11f3b7894ba73b802332d4283
claytonnguyen <claytonnguyen3@gmail.com>
2021-07-07 19:37:00 -0700

    interest calculator working correctly and just needs input for user interactivity

--- src/App.js
+++ src/App.js
@@ -1,39 +1,69 @@
 import './App.css';
 import { Line } from 'react-chartjs-2';
 
+function compoundInterest(initial, months, rate) {
+  const results = [];
+  var total = initial;
+  var interest = 0;
+  results.push(total);
+  for (let i = 0; i < months; i++) {
+    interest = total * rate;
+    total += interest;
+    results.push(total);
+  }
+  return results;
+}
+
+function labelArray(stats) {
+  const length = stats.length;
+  const results = [];
+  for (let i = 0; i < length; i++) {
+    results.push(i);
+  }
+  return results;
+}
+
+const stats = compoundInterest(5000, 60, 0.075);
+
+const months = labelArray(stats);
+
 const state = {
-  labels: ['January', 'February', 'March',
-           'April', 'May'],
+  labels: months,
   datasets: [
     {
-      label: 'Rainfall',
+      label: 'Compound Interest',
       fill: false,
       lineTension: 0.5,
       backgroundColor: 'rgba(75,192,192,1)',
       borderColor: 'rgba(0,0,0,1)',
       borderWidth: 2,
-      data: [65, 59, 80, 81, 56]
+      data: stats
     }
   ]
 }
 
 function App() {
+  
   return (
     <div className="App">
-      <Line
-          data={state}
-          options={{
-            title:{
-              display:true,
-              text:'Average Rainfall per month',
-              fontSize:20
-            },
-            legend:{
-              display:true,
-              position:'right'
-            }
-          }}
-        />
+      <div className="Dashboard">
+        <div className="Line-Graph">
+          <Line
+            data={state}
+            options={{
+              title:{
+                display:true,
+                text:'Average Rainfall per month',
+                fontSize:20
+              },
+              legend:{
+                display:true,
+                position:'right'
+              }
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
