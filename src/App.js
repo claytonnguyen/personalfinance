5875923e239d5b1e0a1c30c03e95075db53f47b2
claytonnguyen <claytonnguyen3@gmail.com>
2021-07-08 16:01:31 -0700

    got the chart to be responsive to user input but the formula to calculate compound interest is wrong

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useState, useEffect } from 'react';
 import './App.css';
 import { Line } from 'react-chartjs-2';
 
@@ -23,30 +24,52 @@ function labelArray(stats) {
   return results;
 }
 
-const stats = compoundInterest(5000, 60, 0.075);
+let stats = [];
 
-const months = labelArray(stats);
-
-const state = {
-  labels: months,
-  datasets: [
-    {
-      label: 'Compound Interest',
-      fill: false,
-      lineTension: 0.5,
-      backgroundColor: 'rgba(75,192,192,1)',
-      borderColor: 'rgba(0,0,0,1)',
-      borderWidth: 2,
-      data: stats
-    }
-  ]
-}
 
 function App() {
-  
+  const [state, setState] = useState();
+
+  const [initial, setInitial] = useState(0);
+  const [months, setMonths] = useState(0);
+  const [rate, setRate] = useState(0);
+  const [annual, setAnnual] = useState("hi");
+
+  useEffect(() => {
+    stats = compoundInterest(initial, months, rate);
+    let count = labelArray(stats);
+    let data = {
+      labels: count,
+      datasets: [
+        {
+          label: 'Compound Interest',
+          fill: false,
+          lineTension: 0.5,
+          backgroundColor: 'rgba(75,192,192,1)',
+          borderColor: 'rgba(0,0,0,1)',
+          borderWidth: 2,
+          data: stats
+        }
+      ]
+    }
+    setState(data);
+  }, [initial, months, rate]);
+
   return (
     <div className="App">
       <div className="Dashboard">
+        <div className="Form">
+          <div>
+            <label htmlFor="initial">Initial Deposit: {initial} </label>
+            <input type="text" name="initial" onChange={(e) => setInitial(Number(e.target.value))} ></input>
+            <label htmlFor="months">Months Invested: {months} </label>
+            <input type="text" name="months" onChange={(e) => setMonths(Number(e.target.value))} ></input>
+            <label htmlFor="rate">Investment rate: {rate} </label>
+            <input type="text" name="rate" onChange={(e) => setRate(Number(e.target.value))} ></input>
+            <label htmlFor="annual">Compounded: {annual} </label>
+            <input type="text" name="annual" onChange={(e) => setAnnual(e.target.value)} ></input>
+          </div>
+        </div>
         <div className="Line-Graph">
           <Line
             data={state}
